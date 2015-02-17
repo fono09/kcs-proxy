e8d82e56f5d3dca678d39535f604646b7b8c2b4c
fono09 <fono@fono.jp>
2015-02-17 19:33:12 +0900

    Parsing JSON

--- kcs-proxy.rb
+++ kcs-proxy.rb
@@ -6,31 +6,17 @@ require 'webrick'
 require 'webrick/httpproxy'
 require 'json'
 
-=begin
-gapi_get_member/ship3
-gapi_req_kaisou/slotset
-gapi_req_hokyu/charge
-gapi_get_member/ndock
-gapi_req_kousyou/createship
-gapi_get_member/kdock
-gapi_get_member/material
-gapi_req_hensei/change
-gapi_get_member/record
-gapi_req_member/updatecomment
-gapi_req_ranking/getlist
-gapi_port/port
-gapi_req_kaisou/powerup
-gapi_req_kousyou/createitem
-=end
+api_list = %w!api_start2  api_port/port  api_get_member/basic api_get_member/furniture api_get_member/slot_item api_get_member/useitem api_get_member/kdock api_get_member/ndock api_get_member/unsetslot api_get_member/ship2 api_get_member/ship3 api_get_member/material api_get_member/record api_get_member/questlist api_get_member/deck api_get_member/sortie_conditions api_get_member/mapcell api_get_member/mapinfo api_get_member/practice api_get_member/mission  api_req_member/get_practice_enemyinfo  api_req_map/start api_req_map/next  api_req_sortie/battle api_req_sortie/battleresult  api_req_mission/start  api_req_practice/battle api_req_practice/battle_result  api_req_quest/start api_req_quest/stop  api_req_member/updatecomment api_req_member/get_incentive  api_req_kaisou/slotset api_req_kaisou/powerup  api_req_hokyu/charge  api_req_kousyou/createitem api_req_kousyou/createship api_req_kousyou/getship  api_req_hensei/change api_req_hensei/lock  api_req_ranking/getlist  api_req_nyukyo/speedchange!
 
-api_list = ['/api_port/port',]
-kcs_handler = Proc.new() {|req,res|
+
+kcs_handler = Proc.new{|req,res|
 
 	if req.unparsed_uri.include?("kcsapi") then
 
-		p req.unparsed_uri
-		print "\n"
-
+		
+		Thread.fork(res.body) {|body|
+			p JSON.parse(body.gsub(/svdata=/,''))
+		}
 
 	end
 	
